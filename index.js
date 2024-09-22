fc35b0370ce6169047a8fd2b09cde58f1ed91443
chandan kanaujia <chandanegc@gmail.com>
2024-09-23 01:17:49 +0530

    for deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.static("build")); 
+app.use(express.static("build")); 
 app.use("/api", router);
 
 app.get("/test", (req, res) => {
