6f665d253781a6b0083372cbbbb1c631642776c8
chandan <chandanegc@gmail.com>
2024-11-22 12:15:10 +0530

    fixedsome issue

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ database();
 
 const port = process.env.PORT || 8000;
 
-app.listen(port,"0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Server is Running on http://localhost:${port}`);
 });
